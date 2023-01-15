770b156aeece12e7a6fd2aab8bcff5b80d4b75ed
jpmunich <parkercoyle8@gmail.com>
2023-01-15 16:15:40 -0500

    Webpack set up

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const div = document.createElement("div");
+div.innerHTML = "Hioefhs";
+document.body.appendChild(div);
+
+const img = document.createElement("img");
+img.src = "images/weather-app-background.jpg";
+document.body.appendChild(img);
